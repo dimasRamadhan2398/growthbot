5eef9c042c4779115e907655e986ca694d68e898
Dimas Saifullah Ramadhan <dimasramadhan9833@gmail.com>
2026-06-18 21:15:09 +0700

    chore: point frontend API to Biznetgio VPS

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import "./index.css";
 
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "";
+
 const app = createApp(App);
 app.use(router);
 app.mount("#app");
